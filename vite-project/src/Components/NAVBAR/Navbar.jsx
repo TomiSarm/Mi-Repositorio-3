05ba5696594136d806a562801f377535582680cb
“Tomas <tomas51342679@gmail.com>
2024-04-17 18:41:33 -0300

    Se modifico el navbar

--- vite-project/src/Components/NAVBAR/Navbar.jsx
+++ vite-project/src/Components/NAVBAR/Navbar.jsx
@@ -4,15 +4,20 @@ import Carrito from "../CARTWIDGET/Cartwidget.jsx"
 const Navbar = () => {
   return (
     <header className={styles.navbar}>
-        <img src={logo} alt="" className= {styles.img}/>
-        <nav>
-            <ul className={styles.elementosNavbar}>
-                <li>MENU</li>
-                <li>NOSOTROS</li>
-                <li>CONTACTO</li>
-            </ul>
+        <nav className="navbar bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand"><img src={logo} alt="Logo" width="150" height="150" className="d-inline-block align-text-top"/></a>
+            <a className="nav-link active" aria-current="page" href="#">Home</a>
+            <a className="nav-link" href="#">Our history</a>
+            <a className="nav-link" href="#">Contact</a>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="Form"/>
+              <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+            <Carrito/>
+          </div>
         </nav>
-        <Carrito/>
+        
     </header>
   )
 }
