bc6e55eb39a8c0f73007a376b12e80ba27af1560
“Tomas <tomas51342679@gmail.com>
2024-04-18 18:11:49 -0300

    Se agrego itemListContainer

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.module.css'
 import Navbar from "./Components/NAVBAR/Navbar.jsx"
+import ItemListContainer from "./Components/ITEMLISTCONTAINER/itemListContainer.jsx"
 
 function App() {
-  
+  const itemListContainerProps = {
+    saludo: "HOLA BIENVENIDO A NUESTRA TIENDA DE HAMBURGUESAS"
+  }
+
   return (
     <>
       <Navbar/>
-      </>
+      <ItemListContainer
+      { ...itemListContainerProps}
+       />
+    </>
   )
 }
 
