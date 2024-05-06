0ce61cef444d5dfbfb3d56f95bdc30c4f91e2c5e
“Tomas <tomas51342679@gmail.com>
2024-05-06 18:44:22 -0300

    a

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,26 @@
 import './App.module.css'
 import Navbar from "./Components/NAVBAR/Navbar.jsx"
-import ItemListContainer from "./Components/ITEMLISTCONTAINER/itemListContainer.jsx"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import CatalogoHamburguesas from './Components/CATA-HAMBUR/CatalogoHamburguesas.jsx';
+import DetalleHamburguesa from './Components/DETALLE-HAMBUR/Detallehamburguesa.jsx';
+import Mockdata from "../Utils/MockData.js";
 
-function App() {
-  const itemListContainerProps = {
-    saludo: "HOLA BIENVENIDO A NUESTRA TIENDA DE HAMBURGUESAS"
-  }
+const hamburguesasClasicas = Mockdata.filter(hamburguesa => hamburguesa.categoria === 'Clasicas');
+const hamburguesasEspeciales = Mockdata.filter(hamburguesa => hamburguesa.categoria === 'Especiales');
 
+const App = () => {
   return (
-    <>
-      <Navbar/>
-      <ItemListContainer
-      { ...itemListContainerProps}
-       />
-    </>
-  )
-}
-
-export default App
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/Clasicas" element={<CatalogoHamburguesas hamburguesas={hamburguesasClasicas} />} />
+          <Route path="/Especiales" element={<CatalogoHamburguesas hamburguesas={hamburguesasEspeciales} />} />
+          <Route path="/detalle/:id" element={<DetalleHamburguesa hamburguesas={Mockdata} />} />
+          <Route path="/" element={<CatalogoHamburguesas hamburguesas={Mockdata} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+export default App;
--- vite-project/src/Components/NAVBAR/Navbar.jsx
+++ vite-project/src/Components/NAVBAR/Navbar.jsx
@@ -1,25 +1,27 @@
 import logo from "../../assets/Logo.jpg"
 import styles from "./Navbar.module.css"
-import Carrito from "../CARTWIDGET/Cartwidget.jsx"
+import React from 'react';
+import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <header className={styles.navbar}>
-        <nav className="navbar bg-body-tertiary">
-          <div className="container-fluid">
-            <a className="navbar-brand"><img src={logo} alt="Logo" width="150" height="150" className="d-inline-block align-text-top"/></a>
-            <a className="nav-link active" aria-current="page" href="#">Home</a>
-            <a className="nav-link" href="#">Our history</a>
-            <a className="nav-link" href="#">Contact</a>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" id="Form"/>
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-            <Carrito/>
-          </div>
-        </nav>
-        
+      <nav className="navbar bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <img src={logo} alt="Logo" width="150" height="150" className="d-inline-block align-text-top"/>
+          </Link>
+          <Link className="nav-link active" aria-current="page" to="/">Inicio</Link>
+          <Link className="nav-link" to="/Clasicas">Clásicas</Link>
+          <Link className="nav-link" to="/Especiales">Especiales</Link>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" id="Form"/>
+            <button className="btn btn-outline-success" type="submit">Buscar</button>
+          </form>
+        </div>
+      </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
